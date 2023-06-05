b3a73be48b31f8eb4fadd29f6eee6be4bcd2e610
Toni Akintola <108356218+toni-akintola@users.noreply.github.com>
2023-06-05 20:42:40 +0000

    configured eslint & prettier

--- src/app/auth-pages/create-account.tsx
+++ src/app/auth-pages/create-account.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-import { Button, Container, Typography } from '@mui/material'
-
+import React from "react";
+import { useRouter } from "next/router";
+import { Button, Container, Typography } from "@mui/material";
+import Card from "@mui/material";
 export const CreateAccount: React.FC<{}> = () => {
   //user routed to ... screen on click of ... button
-    const router = useRouter();
-    const handleClick = (e: { preventDefault: () => void; }) => {
-    e.preventDefault()
-    router.push('/settings')
-  }
+  const router = useRouter();
+  const handleClick = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    router.push("/settings");
+  };
   return (
-    <Container sx={{alignContent: "center"}}>
+    <Container sx={{ alignContent: "center" }}>
       <Button onClick={handleClick}>Continue as Guest</Button>
       <Typography>Welcome!</Typography>
     </Container>
-  )
+  );
 };
 
 export default CreateAccount;
--- src/app/auth-pages/enter-password.tsx
+++ src/app/auth-pages/enter-password.tsx
@@ -1,47 +1,61 @@
-import React from 'react'
-import { Button, Container, TextField, Grid, Typography, Paper } from '@mui/material'
-import { useForm } from 'react-hook-form';
-import { useRouter } from 'next/router'
-import { yupResolver } from '@hookform/resolvers/yup';
-import { passwordSchema } from '@/utils/schemas';
-import LockIcon from '@mui/icons-material/Lock';
+import React from "react";
+import {
+  Button,
+  Container,
+  TextField,
+  Grid,
+  Typography,
+  Paper,
+} from "@mui/material";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { passwordSchema } from "@/utils/schemas";
+import LockIcon from "@mui/icons-material/Lock";
 
-
-export const EnterPassword: React.FC<{}> = () => {  
-  const { register, handleSubmit, formState: { errors }, } = useForm({
-    resolver: yupResolver(passwordSchema),})
+export const EnterPassword: React.FC<{}> = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(passwordSchema),
+  });
   //user routed to SignIn screen on click of 'return to previous screen' button
-  const router = useRouter()
+  const router = useRouter();
 
-
-  const handleReturnClick = (e: { preventDefault: () => void; }) => {
-    e.preventDefault()
-    router.push('/sign-in') 
-  }
+  const handleReturnClick = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    router.push("/sign-in");
+  };
 
   const submitPassword = (data: any) => {
-    console.log(data.password)
-    router.push('/settings')
-
-  }
+    console.log(data.password);
+    router.push("/settings");
+  };
   return (
     <form onSubmit={handleSubmit(submitPassword)}>
-      <Grid 
+      <Grid
         container
         spacing={0}
         direction="column"
         alignItems="center"
         justifyContent="center"
-        sx={{minHeight: "100vh"}}>
-        <LockIcon sx={{fontSize: 200}}></LockIcon>
+        sx={{ minHeight: "100vh" }}
+      >
+        <LockIcon sx={{ fontSize: 200 }}></LockIcon>
         <Typography>Enter your password to login.</Typography>
         <br></br>
-        <TextField variant='outlined' label='Input your password' {...register('password')}></TextField>
-        <Button type='submit'>Login</Button>
+        <TextField
+          variant="outlined"
+          label="Input your password"
+          {...register("password")}
+        ></TextField>
+        <Button type="submit">Login</Button>
         <Button onClick={handleReturnClick}>Return to previous screen</Button>
       </Grid>
     </form>
-  )
-}
+  );
+};
 
 export default EnterPassword;
