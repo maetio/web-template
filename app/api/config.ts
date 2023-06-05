d2452934d01648a82fe53149004e931f618c6e9b
kekoawong <kwong6@nd.edu>
2023-06-05 16:49:22 -0400

    reverted back to initial firebase web provider

--- app/api/config.ts
+++ app/api/config.ts
@@ -2,6 +2,7 @@ import { FirebaseOptions, getApps, initializeApp } from 'firebase/app';
 import { initializeAuth } from 'firebase/auth';
 import { initializeFirestore } from 'firebase/firestore';
 
+// get the firebase config
 const firebaseConfig: FirebaseOptions = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -11,6 +12,29 @@ const firebaseConfig: FirebaseOptions = {
     appId: process.env.FIREBASE_APP_ID,
     measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
+
+/**
+ * Initialize firestore
+ */
+// export const firestore = initFirestore({
+//     credential: cert(firebaseConfig)
+// });
+
+/** 
+ * Initialize next auth
+ * Example: https://next-auth.js.org/v3/adapters/firebase#options
+ * Firestore Adapter: https://authjs.dev/reference/adapter/firebase
+*/
+// export default NextAuth({
+//   // https://next-auth.js.org/configuration/providers
+//     providers: [
+//         GoogleProvider({
+//             clientId: process.env.GOOGLE_ID,
+//             clientSecret: process.env.GOOGLE_SECRET,
+//         }),
+//     ],
+//     adapter: FirestoreAdapter(firestore),
+// });
 
 /**
  * Initialize all the firebase apps
