348764128956679f8b93fd8f7b52cad8589abea3
kekoawong <kwong6@nd.edu>
2023-06-06 11:51:04 -0400

    Added error boundry

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <MuiProvider>
-          {children}
-        </MuiProvider>
+          <MuiProvider>
+            {children}
+          </MuiProvider>
       </body>
     </html>
   );
