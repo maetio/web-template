e9af5d18f7ee0203802d9af5584ad2b0264f8837
kekoawong <kwong6@nd.edu>
2023-06-07 10:10:35 -0400

    Firestore configuration working

--- app/api/auth/index.ts
+++ app/api/auth/index.ts
@@ -1,4 +1,4 @@
-import { app } from 'app/api/config';
+import { FirebaseOptions, initializeApp, getApps } from 'firebase/app';
 import { ActionCodeSettings, sendSignInLinkToEmail, initializeAuth } from 'firebase/auth';
 // import { initializeFirestore } from 'firebase/firestore';
 // import NextAuth from "next-auth";
@@ -6,6 +6,22 @@ import { ActionCodeSettings, sendSignInLinkToEmail, initializeAuth } from 'fireb
 // import GoogleProvider from "next-auth/providers/google";
 // import { initFirestore } from '@next-auth/firebase-adapter';
 
+// get the firebase config
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+/**
+ * Initialize all the firebase apps and the auth
+ */
+const allApps = getApps();
+export const app = allApps.length === 0 ? initializeApp(firebaseConfig) : allApps[0];
 export const auth = initializeAuth(app);
 
 /**
--- app/api/config.ts
+++ app/api/config.ts
@@ -1,20 +1,7 @@
-import { FirebaseOptions, initializeApp, getApps } from 'firebase/app';
 // import { initializeFirestore } from 'firebase/firestore';
-import NextAuth from 'next-auth';
-import { FirestoreAdapter, initFirestore } from '@next-auth/firebase-adapter';
-import GoogleProvider from 'next-auth/providers/google';
+import { initFirestore } from '@next-auth/firebase-adapter';
+// import GoogleProvider from 'next-auth/providers/google';
 import { cert } from 'firebase-admin/app';
-
-// get the firebase config
-const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
 
 /**
  * Initialize firestore with next
@@ -34,20 +21,15 @@ export const firestore = initFirestore({
  * Example: https://next-auth.js.org/v3/adapters/firebase#options
  * Firestore Adapter: https://authjs.dev/reference/adapter/firebase
  */
-export default NextAuth({
-  // https://next-auth.js.org/configuration/providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    }),
-  ],
-  adapter: FirestoreAdapter(firestore),
-});
-/**
- * Initialize all the firebase apps
- */
-const allApps = getApps();
-export const app = allApps.length === 0 ? initializeApp(firebaseConfig) : allApps[0];
+// export default NextAuth({
+//   // https://next-auth.js.org/configuration/providers
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID || '',
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+//     }),
+//   ],
+//   adapter: FirestoreAdapter(firestore),
+// });
 
 // export const firestore = initializeFirestore(app, {});
