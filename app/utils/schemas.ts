014a0c70f74c78345ec3855469a8275f72a1c637
toni-akintola <oakintol@nd.edu>
2023-06-08 18:04:27 -0400

    added competitionFormSchema

--- app/utils/schemas.ts
+++ app/utils/schemas.ts
@@ -16,3 +16,16 @@ export const passwordSchema = yup.object().shape({
 });
 
 export type passwordSchemaType = yup.InferType<typeof passwordSchema>
+
+export const competitionFormSchema = yup.object().shape({
+  competitionName: yup.string().required('Session name is required'),
+  sport: yup.string().required('Sport selection is required'),
+  competitionType: yup.string().required('Competition Type is required'),
+  startDate: yup.date().required('Start Date is required'),
+  startTime: yup.date().required('Start Time is required'),
+  endDate: yup.date().required('End Date is required'),
+  endTime: yup.date().required('End Time is required'),
+  location: yup.string().required('Location is required')
+})
+
+export type competitionFormSchema = yup.InferType<typeof competitionFormSchema>
