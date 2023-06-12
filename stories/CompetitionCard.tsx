bb5c22135412b489084229a5f33ee5440d76815a
toni-akintola <oakintol@nd.edu>
2023-06-12 16:47:11 -0400

    added box for image

--- stories/CompetitionCard.tsx
+++ stories/CompetitionCard.tsx
@@ -1,5 +1,5 @@
 import { Link, SportsBasketball } from "@mui/icons-material";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, ButtonBase, Grid, Typography } from "@mui/material";
 import { CalendarIcon, ClockIcon } from "@mui/x-date-pickers";
 import React from "react";
 
@@ -7,24 +7,38 @@ type Props = {}
 
 export const CompetitionCard = (props: Props) => {
 	return (
-		<Box sx={{
-			backgroundColor: "#f5f5f4",
-			border: 1,
-			borderRadius: 2,
-			borderColor: "#f5f5f4",
-			flex: "flex-inline"
-		}}>
+		<Grid
+			container
+			direction="row"
+			spacing={2}
+			justifyContent="flex-start"
+			alignItems="center" 
+			sx={{
+				backgroundColor: "#f5f5f4",
+				border: 1,
+				borderRadius: 2,
+				borderColor: "#f5f5f4",
+				flex: "flex-inline"
+			}}>
+			<Grid 
+				container 
+				item xs={2}
+				alignItems="center"
+			>
+				<ButtonBase sx={{ width: 100, height: 100, border: 1, borderRadius: 1, 
+					borderColor: "#f5f5f4", backgroundColor: "purple", m: 1 }}>
+            
+				</ButtonBase>
+			</Grid>
 			<Grid
+				item xs
 				container
 				direction="column"
 			>
 				<Typography sx={{fontWeight: 400}}>
 				Grand Prairie Youth Rec Tournament
 				</Typography>
-				<Box sx={{
-                    
-				}}
-				></Box>
+				
 				<Box
 					sx={{
 						display: "flex"
@@ -34,13 +48,13 @@ export const CompetitionCard = (props: Props) => {
 					<Typography>Basketball Tournament</Typography>
 				</Box>
 				<Box sx={{
-					flexDirection: "column-reverse",
 					display: "flex" 
 				}}>
-                    April 7
+					<CalendarIcon></CalendarIcon>
+					<Typography>April 7</Typography>
 				</Box>
 			</Grid>
-		</Box>
+		</Grid>
 	);
 };
 
