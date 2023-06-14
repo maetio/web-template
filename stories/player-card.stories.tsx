f319245a77be900a853757cf4e487fb9ec5685e5
toni-akintola <oakintol@nd.edu>
2023-06-14 15:26:28 -0400

    team & player cards complete

---
+++ stories/player-card.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import PlayerCard from "./player-card";
+
+const meta: Meta<typeof PlayerCard> = {
+	/* 👇 The title prop is optional.
+	 * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+	 * to learn how to generate automatic titles
+	 */
+	title: "PlayerCard",
+	component: PlayerCard,
+	argTypes: {
+		
+	}
+};
+
+export default meta;
+type Story = StoryObj<typeof PlayerCard>;
+
+export const Card: Story = {
+	args: {
+	}
+};
