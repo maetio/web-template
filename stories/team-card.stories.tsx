7ad25a33e6aa2bae9d67f9d6ee40b264fe206647
toni-akintola <oakintol@nd.edu>
2023-06-14 12:58:28 -0400

    created and configured team card component

--- stories/team-card.stories.tsx
+++ stories/team-card.stories.tsx
@@ -1,21 +1,20 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { CompetitionCard, CompetitionCardProps } from "./competition-card";
+import TeamCard from "./team-card";
 
-
-const meta: Meta<typeof CompetitionCard> = {
+const meta: Meta<typeof TeamCard> = {
 	/* 👇 The title prop is optional.
 	 * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
 	 * to learn how to generate automatic titles
 	 */
-	title: "CompetitionCard",
-	component: CompetitionCard,
+	title: "TeamCard",
+	component: TeamCard,
 	argTypes: {
 		
 	}
 };
 
 export default meta;
-type Story = StoryObj<typeof CompetitionCard>;
+type Story = StoryObj<typeof TeamCard>;
 
 export const Card: Story = {
 	args: {
---
+++ stories/team-card.tsx
@@ -0,0 +1,14 @@
+import { Grid } from "@mui/material";
+import React from "react";
+
+type TeamCardProps = {
+
+}
+
+const TeamCard = (props: TeamCardProps) => {
+	return (
+		<Grid></Grid>
+	);
+};
+
+export default TeamCard;
