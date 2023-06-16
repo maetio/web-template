d33f51edd59aa03c4b37ec2922ef72fdc3f322be
toni-akintola <oakintol@nd.edu>
2023-06-16 13:08:50 -0400

    fixed storybook node module error (hooray!)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,8 +13,31 @@ const config: StorybookConfig = {
 		name: "@storybook/nextjs",
 		options: {},
 	},
+	core: {
+		builder: {
+			name: '@storybook/builder-webpack5',
+			options: {
+			  fsCache: true,
+			  lazyCompilation: true,
+			},
+		},
+	},
 	docs: {
 		autodocs: "tag",
 	},
+	webpackFinal: async (config, { configType }) => {
+        config.resolve = {
+            ...config.resolve,
+            fallback: {
+                ...(config.resolve || {}).fallback,
+                fs: false,
+                stream: false,
+                os: false,
+            },
+        }
+
+        // Return the altered config
+        return config
+    },
 };
 export default config;
