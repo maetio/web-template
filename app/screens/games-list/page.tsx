a54811c6cc1c78fdc5c79cd2a5a889a4a1d4f9f7
toni-akintola <oakintol@nd.edu>
2023-06-19 12:48:44 -0400

    completed all offshoots of ViewCompetition screen

---
+++ app/screens/games-list/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {}
+
+const GamesList = (props: Props) => {
+	return (
+		<div>
+			<h1>Games List</h1>
+		</div>
+	);
+};
+
+export default GamesList;
--- app/screens/view-competition/page.tsx
+++ app/screens/view-competition/page.tsx
@@ -7,7 +7,14 @@ const ViewCompetition = (props: Props) => {
 	return (
 		<div>
 			<h1>View Competition</h1>
-			<Link href="/screens/team-rankings">Team Rankings</Link>
+			<div>
+				<Link href="/screens/create-game">Create Game </Link>
+				<Link href="/screens/edit-competition">Edit Competition </Link>
+				<Link href="/screens/input-scores">Input Scores </Link>
+				<Link href="/screens/team-rankings">Team Rankings </Link>
+				<Link href="/screens/player-rankings">Player Rankings </Link>
+				<Link href="/screens/games-list">Games List </Link>
+			</div>
 		</div>
 	);
 };
