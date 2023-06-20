c1b5373add4c2e440bc1ab437727f68efadef3d9
toni-akintola <oakintol@nd.edu>
2023-06-20 17:45:06 -0400

    working react hook form for user first & last name

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import React, { useEffect } from "react";
-import { Grid, TextField, Typography } from "@mui/material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
 import { signInWithLink } from "app/api/auth";
 import { useRecoilValue } from "recoil";
 import { UserState } from "app/recoil-store";
 import { SignOutButton } from "app/components/sign-out-button";
 import { useAuthContext } from "app/components/providers/auth-context";
+import { useForm } from "react-hook-form";
 
 /**
  * Will have the home screen render
@@ -14,6 +15,20 @@ import { useAuthContext } from "app/components/providers/auth-context";
  * @return {*}
  */
 const Home = () => {
+	
+	// set up react hook form that will take user firstName and lastName as inputs
+	const { register, handleSubmit } = useForm({
+		defaultValues: {
+			firstName: "",
+			lastName: ""
+		}
+	});
+
+	// asynchronous function that handles user submission of form
+	const onSubmit = async (data: { firstName: string, lastName: string }) => {
+		console.log(data);
+	};	
+
 	// get user state
 	const user = useRecoilValue(UserState);
 
@@ -26,23 +41,27 @@ const Home = () => {
 	const userContext = useAuthContext();
 
 	return (
-		<Grid
-			container
-			spacing={0}
-			direction="column"
-			alignItems="center"
-			justifyContent="center"
-			sx={{ minHeight: "100vh" }}
-		>
-			<Typography>
-				{userContext?.uid.length
-					? `You are logged in as ${userContext?.email}.`
-					: "You are not logged in."}
-			</Typography>
-			<TextField label="First Name"></TextField>
-			<TextField label="Last Name"></TextField>
-			<SignOutButton />
-		</Grid>
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<Grid
+				container
+				spacing={0}
+				direction="column"
+				alignItems="center"
+				justifyContent="center"
+				sx={{ minHeight: "100vh" }}
+			>
+				<Typography>
+					{userContext?.uid.length
+						? `You are logged in as ${userContext?.email}.`
+						: "You are not logged in."}
+				</Typography>
+				<TextField {...register("firstName", { required: true })} sx={{m: 3}} label="First Name"></TextField>
+				<TextField {...register("lastName", { required: true })} label="Last Name"></TextField>
+				<Button variant="outlined" type="submit" sx={{m: 2}}>Submit</Button>
+
+				<SignOutButton />
+			</Grid>
+		</form>
 	);
 };
 
