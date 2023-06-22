4b0a17163c4ce253a53d63d9863722417704a85b
toni-akintola <oakintol@nd.edu>
2023-06-21 23:19:54 -0400

    styled textfield

--- app/components/textfield.tsx
+++ app/components/textfield.tsx
@@ -1,11 +1,12 @@
 import { Box, Typography } from "@mui/material";
+import { M_PLUS_1 } from "next/font/google";
 import React from "react";
 
 type Props = {};
 
 export const TextField = (props: Props) => {
 	return(
-		<Box>
+		<Box sx={{backgroundColor: "#D9D9D9", border: 1, borderColor: "#D9D9D9", borderRadius: 15}}>
 			<Typography>Input Field</Typography>
 		</Box>
 	) ;
