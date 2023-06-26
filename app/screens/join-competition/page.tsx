786d996e3affb86e03235f76a5141d25d95c2a88
toni-akintola <oakintol@nd.edu>
2023-06-26 10:14:38 -0400

    created a join competition screen

---
+++ app/screens/join-competition/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type JoinCompetitionProps = {}
+
+const JoinCompetition = (props: JoinCompetitionProps) => {
+	return (
+		<div>JoinCompetition</div>
+	);
+};
+
+export default JoinCompetition;
