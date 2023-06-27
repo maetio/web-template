03142caf4f9f4b8b072c2d0a589d1758064e6d9f
toni-akintola <oakintol@nd.edu>
2023-06-27 15:03:52 -0400

    working registers for all input fields

--- app/screens/user-input/create-team/page.tsx
+++ app/screens/user-input/create-team/page.tsx
@@ -10,6 +10,8 @@ import {
 import { PlayerCard } from "app/components/cards/player-card/page";
 import { PlayerDropdown } from "app/components/user-input/player-dropdown/page";
 import { SubmitButton } from "app/components/user-input/submit-button/page";
+import { useForm } from "react-hook-form";
+
 import React from "react";
 
 export interface CreateTeamProps {
@@ -18,69 +20,74 @@ export interface CreateTeamProps {
 }
 
 export const CreateTeam: React.FC<CreateTeamProps> = ({name, image}) => {
+
+	const { register, handleSubmit, formState: { errors } } = useForm();
+
 	return (
-		<Grid
-			sx={{ height: "100vh", backgroundColor: "#D9D9D9" }}
-			container
-			alignItems="center"
-			justifyContent="center"
-		>
+		<form onSubmit={handleSubmit((data) => console.log(data))}>
 			<Grid
-				sx={{
-					width: 1100,
-					height: 850,
-					border: 1,
-					borderColor: "#FAFAFA",
-					borderRadius: 30,
-					backgroundColor: "#FAFAFA",
-				}}
+				sx={{ height: "100vh", backgroundColor: "#D9D9D9" }}
 				container
 				alignItems="center"
+				justifyContent="center"
 			>
 				<Grid
-					container
-					direction="row"
-					justifyContent="flex-end"
-					alignItems="flex-end"
 					sx={{
-						width: 900,
+						width: 1100,
+						height: 850,
+						border: 1,
+						borderColor: "#FAFAFA",
+						borderRadius: 30,
+						backgroundColor: "#FAFAFA",
 					}}
+					container
+					alignItems="center"
 				>
-					<Cancel></Cancel>
-				</Grid>
-				<Grid container direction="column" alignItems="center" item>
 					<Grid
 						container
 						direction="row"
-						alignItems="center"
-						justifyContent="center"
+						justifyContent="flex-end"
+						alignItems="flex-end"
 						sx={{
-							mb: 1,
-							backgroundColor: "#D9D9D9",
-							width: 250,
-							height: 250,
-							borderRadius: 4,
+							width: 900,
 						}}
 					>
-						<Panorama sx={{ m: 1 }}></Panorama>
-						<Typography>Upload a picture</Typography>
+						<Cancel></Cancel>
 					</Grid>
-					<InputField id="teamName" label="Team Name"></InputField>
-					<InputField id="teamName" label="Team Location"></InputField>
-					<Grid
-						container
-						direction="column"
-						sx={{ mt: 1, mb: 3, width: 480 }}
-					>
-						<PlayerDropdown />
+					<Grid container direction="column" alignItems="center" item>
+						<Grid
+							container
+							direction="row"
+							alignItems="center"
+							justifyContent="center"
+							sx={{
+								mb: 1,
+								backgroundColor: "#D9D9D9",
+								width: 250,
+								height: 250,
+								borderRadius: 4,
+							}}
+						>
+							<Panorama sx={{ m: 1 }}></Panorama>
+							<Typography>Upload a picture</Typography>
+						</Grid>
+						<InputField id="teamName" register={register} label="Team Name"></InputField>
+						<InputField id="teamLocation" register={register} label="Team Location"></InputField>
+						<Grid
+							container
+							direction="column"
+							sx={{ mt: 1, mb: 3, width: 480 }}
+						>
+							<PlayerDropdown />
+						</Grid>
+						<SubmitButton
+							color="#818CF8"
+							title="Create Team"
+						/>
 					</Grid>
-					<SubmitButton
-						color="#818CF8"
-						title="Create Team"
-					/>
 				</Grid>
 			</Grid>
-		</Grid>
+		</form>
 	);
 };
 
