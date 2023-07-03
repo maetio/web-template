fe3a70c7d8513473a95836eb48f90e9c72d3091c
toni-akintola <oakintol@nd.edu>
2023-07-03 14:01:49 -0400

    created a ViewCompetition page

--- app/screens/view/view-competition/page.tsx
+++ app/screens/view/view-competition/page.tsx
@@ -1,21 +1,18 @@
-import Link from "next/link";
 import React from "react";
 
-// export interface ViewCompetitionProps = {};
+// export interface ViewCompetitionProps {};
 
-export const ViewCompetition: React.FC<{}> = () => {
+export 
+/**
+ * This screen shows competition information, top players & teams for the competition,
+ * & games on the competition schedule
+ * 
+ * @param {ViewCompetitionProps} props 
+ * @returns 
+ */
+const ViewCompetition = (props: ViewCompetitionProps) => {
 	return (
-		<div>
-			<h1>View Competition</h1>
-			<div>
-				<Link href="/screens/create-game">Create Game </Link>
-				<Link href="/screens/edit-competition">Edit Competition </Link>
-				<Link href="/screens/input-scores">Input Scores </Link>
-				<Link href="/screens/team-rankings">Team Rankings </Link>
-				<Link href="/screens/player-rankings">Player Rankings </Link>
-				<Link href="/screens/games-list">Games List </Link>
-			</div>
-		</div>
+		<div>ViewCompetition</div>
 	);
 };
 
