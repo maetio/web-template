0ec2c07b159ceea107116aa8d05cd2fe996db557
toni-akintola <oakintol@nd.edu>
2023-07-03 14:38:56 -0400

    working through top third of view comp ui

--- app/screens/view/view-competition/page.tsx
+++ app/screens/view/view-competition/page.tsx
@@ -1,3 +1,4 @@
+import { Box, Grid, Typography } from "app/components/providers/mui-server-components";
 import React from "react";
 
 // export interface ViewCompetitionProps {};
@@ -12,7 +13,43 @@ export
  */
 const ViewCompetition = (props: ViewCompetitionProps) => {
 	return (
-		<div>ViewCompetition</div>
+		<Grid
+			container
+			direction="row"
+			sx={{ml: 10, mr: 10}}
+		>
+			<Grid
+				container
+				item
+				justifyContent="flex-start"
+				direction="row"
+			>
+				<Grid
+					container
+					item
+					direction="row"
+				>
+					<Typography sx={{fontWeight: 700}} variant="h5">Competition Name</Typography>
+					<Typography sx={{ml: 3, fontWeight: 700}}>Host Name</Typography>
+				</Grid>
+				<Grid
+			        container
+					item
+					direction="row"
+				>
+					<Box
+						sx={{
+							mb: 1,
+							backgroundImage: "linear-gradient(180deg, #908EEA 0%, #BEFBE5 100%)",
+							width: 250,
+							height: 250,
+							borderRadius: 4,
+						}}
+					></Box>
+				</Grid>
+				
+			</Grid>
+		</Grid>
 	);
 };
 
