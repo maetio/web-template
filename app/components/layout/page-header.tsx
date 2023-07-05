2aeecaa6e9823a7c69bfb8142b0ec9f377c18b17
toni-akintola <oakintol@nd.edu>
2023-07-05 15:16:07 -0400

    scrapped old page header

--- app/components/layout/page-header.tsx
+++ app/components/layout/page-header.tsx
@@ -3,7 +3,7 @@ import { Grid, Typography, Divider, Box } from "@mui/material";
 import React from "react";
 
 export interface PageHeaderProps {
-	title: string;
+	title?: string;
 }
 
 export /**
@@ -15,25 +15,5 @@ export /**
  *  @return {*}
  *
  */ const PageHeader = (props: PageHeaderProps) => (
-	<Box
-		justifyContent="center"
-		alignItems={"center"}
-		sx={{
-			display: "flex",
-			textAlign: "center",
-			backgroundColor: "#4f46e5",
-			border: 1,
-			borderRadius: 1,
-			borderColor: "#4f46e5",
-		}}
-	>
-		<Typography
-			fontFamily={["Nunito", "sans-serif"]}
-			color="white"
-			variant="h5"
-			fontWeight="800"
-		>
-			{props.title}
-		</Typography>
-	</Box>
+	
 );
