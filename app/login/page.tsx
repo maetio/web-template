ece8617271f15ecadd66cc72f251813f5c2b124a
toni-akintola <oakintol@nd.edu>
2023-07-10 13:25:58 -0400

    edited email & magic link

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -33,27 +33,31 @@ const LoginPage: React.FC<{}> = () => {
 	};
 
 	return (
-		<form onSubmit={handleSubmit(submitEmail)} className="flex gap-2 items-center">
-			<input
-				// eslint-disable-next-line react/jsx-props-no-spreading
-				{...register("email")}
-				required
-				type="text"
-				name="email"
-				className="text-2xl p-1 rounded-lg flex-grow w-full"
-				placeholder="Enter Email"
-				autoFocus
-			/>
-			{sentEmail ?
-				<div>Sent!</div> : 			
-				<button
-					type="submit"
-					className="p-2 mt-4 text-xl rounded-2xl text-black border-solid border-black border-2 max-w-xs bg-primaryMain hover:cursor-pointer hover:bg-primaryMainLight"
-				>
+		<div className="flex max-h-full max-w-full items-center">
+			<div className="inline-block h-40 bg-lightGray items-center justify-center">
+				<form onSubmit={handleSubmit(submitEmail)} className="flex gap-2 items-center">
+					<input
+						// eslint-disable-next-line react/jsx-props-no-spreading
+						{...register("email")}
+						required
+						type="text"
+						name="email"
+						className="text-2xl p-1 rounded-lg flex-grow w-50"
+						placeholder="Enter Email"
+						autoFocus
+					/>
+					{sentEmail ?
+						<div>Sent!</div> : 			
+						<button
+							type="submit"
+							className="p-2 mt-4 mr-4 text-xl rounded-2xl text-black border-solid border-black border-2 max-w-xs bg-primaryMain hover:cursor-pointer hover:bg-primaryMainLight"
+						>
 					Send Magic Link
-				</button>
-			}
-		</form>
+						</button>
+					}
+				</form>
+			</div>
+		</div>
 	);
 };
 
