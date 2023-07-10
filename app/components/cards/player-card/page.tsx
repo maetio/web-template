a122f3252aa4e0397d329a44a45737fac25876d7
toni-akintola <oakintol@nd.edu>
2023-07-10 16:29:56 -0400

    started on playerCard

--- app/components/cards/player-card/page.tsx
+++ app/components/cards/player-card/page.tsx
@@ -19,7 +19,15 @@ export /**
 const PlayerCard: React.FC<PlayerCardProps> = ({ name, image, score }) => {
 	return (
 		<div className="grid gap-4 grid-cols-12 justify-start items-center border-b">
-			
+			<div className="col-span-1">
+				<div className=" w-8 h-8 rounded-full ml-4 bg-gradient-to-b from-gradientYellow via-gradientOrange to-gradientBlue"></div>
+			</div>
+			<div className="col-span-2">
+				<text>Player Name</text>
+			</div>
+			<div>
+				<MaetIcon/>
+			</div>
 		</div>
 		// <Grid
 		// 	container
