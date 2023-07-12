b17e1477a7ac6dcd369f5e995ef36e49d80a614d
toni-akintola <oakintol@nd.edu>
2023-07-12 11:00:05 -0400

    added competition card component

---
+++ app/components/cards/CompetitionCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export interface CompetitionCardProps {
+    competitionName?: string
+}
+
+const CompetitionCard: React.FC<CompetitionCardProps> = () => {
+	return (
+		<div>CompetitionCard</div>
+	);
+};
+
+export default CompetitionCard;
