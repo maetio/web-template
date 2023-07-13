5063c510c665769beb281fc6af5572d71612ba25
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2023-07-13 16:53:03 -0400

    testing without middleware

--- app/api/stripe/test2/route.ts
+++ app/api/stripe/test2/route.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req: Request) {
+	const origin = req.headers.get("origin");
 	const message = {
 		message: "hello from get ",
+		origin,
 	};
 
 	return NextResponse.json(message);
