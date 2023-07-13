dde7abfa44103eee414388bef99a9f7b69ee7da5
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2023-07-13 16:55:56 -0400

    removed no origin code

--- middleware.ts
+++ middleware.ts
@@ -55,20 +55,20 @@ const { setCustomUserClaims, getUser } = getFirebaseAuth(
 export async function middleware(request: NextRequest) {
 	const origin = request.headers.get("origin");
 
-	// if (
-	// 	request.nextUrl.pathname.startsWith("/api/stripe/test1") ||
-	// 	request.nextUrl.pathname.startsWith("/api/stripe/create-stripe-account")
-	// ) {
-	// 	if ((origin && !allowedOrigins.includes(origin)) || !origin) {
-	// 		return new NextResponse(null, {
-	// 			status: 400,
-	// 			statusText: "Bad Request",
-	// 			headers: {
-	// 				"Content-Type": "text/plain",
-	// 			},
-	// 		});
-	// 	}
-	// }
+	if (
+		request.nextUrl.pathname.startsWith("/api/stripe/test1") ||
+		request.nextUrl.pathname.startsWith("/api/stripe/create-stripe-account")
+	) {
+		if (origin && !allowedOrigins.includes(origin)) {
+			return new NextResponse(null, {
+				status: 400,
+				statusText: "Bad Request",
+				headers: {
+					"Content-Type": "text/plain",
+				},
+			});
+		}
+	}
 
 	return authentication(request, {
 		// these api routes are automatically created by the middleware
