d646111f608c41d042e972df30c3b8d30938abd1
kekoawong <kwong6@nd.edu>
2023-07-13 10:59:13 -0400

    Completed comp redirect

--- app/join-comp/[id]/page.tsx
+++ app/join-comp/[id]/page.tsx
@@ -40,48 +40,44 @@ export default async function JoinCompScreen({
 		  )
 		: null;
 
-	if (profileData?.id) redirect(`/confirm-comp/${params.id}`);
-
 	/**
 	 * Define the submitting form action
 	 *
 	 * @param {FormData} data
 	 */
-	// const submitFormAction = async () => {
-	// 	"use server";
+	const submitFormAction = async () => {
+		"use server";
 
-	// 	try {
-	// 		// update the data with the server action
-	// 		if (profileData?.id) {
-	// 			await addCompetitionProfile(
-	// 				params.id,
-	// 				competitionData?.sport || "basketball",
-	// 				profileData?.id
-	// 			);
-	// 			redirect(`/confirm-comp/${params.id}`);
-	// 		}
-	// 		console.log('after without profiel data');
-	// 	} catch (e: any) {
-	// 		console.warn("error with form action", e);
-	// 	}
-	// 	console.log('joingin competition');
-	// };
+		try {
+			// update the data with the server action
+			if (profileData?.id) {
+				await addCompetitionProfile(
+					params.id,
+					competitionData?.sport || "basketball",
+					profileData?.id
+				);
+				redirect(`/confirm-comp/${params.id}`);
+			}
+			console.log('after without profiel data');
+		} catch (e: any) {
+			console.warn("error with form action", e);
+		}
+	};
 
 	return (
 		<main>
 			<h1>Competition Name: {competitionData?.name}</h1>
 			<br />
-			<h3>Loading...</h3>
-			{/* <h3>
+			<h3>
 				Join the competition as {profileData?.firstName}{" "}
 				{profileData?.lastName}
 			</h3>
 			<h3>Rating: {profileData?.rating?.displayRating}</h3>
 			<h3>Sport: {profileData?.sport}</h3>
-			<br /> */}
-			{/* <form action={submitFormAction}>
+			<br />
+			<form action={submitFormAction}>
 				<button type="submit">Join Competition</button>
-			</form> */}
+			</form>
 		</main>
 	);
 }
