0c1ec288de04aa888f4a50deafce96e20d2939d7
toni-akintola <oakintol@nd.edu>
2023-07-13 10:07:10 -0400

    touched up CompetitionCard

--- app/components/cards/CompetitionCard.tsx
+++ app/components/cards/CompetitionCard.tsx
@@ -18,7 +18,7 @@ const CompetitionCard: React.FC<CompetitionCardProps> = ({competitionName, sport
 			<div className="grid grid-rows-12 pl-3 pt-3 pr-2">
 				<div className="grid row-span-4 grid-cols-12">
 					<div className="col-span-2 flex">
-						<div className="2xl:w-36 2xl:h-36 w-4/5 h-12 md:min-h-full rounded-md bg-gradient-to-b from-gradientYellow via-gradientOrange to-gradientBlue"></div>
+						<div className="2xl:w-36 2xl:h-36 lg:h-24 lg:w-24 lg:min-h-0 w-4/5 h-12 md:min-h-full rounded-md bg-gradient-to-b from-gradientYellow via-gradientOrange to-gradientBlue"></div>
 					</div>
 					<div className="col-span-7 flex-col flex">
 						<h2 className="font-bold lg:text-3xl sm:text-lg">{competitionName}</h2>
--- app/components/cards/PlayerCard.tsx
+++ app/components/cards/PlayerCard.tsx
@@ -21,7 +21,7 @@ const PlayerCard: React.FC<PlayerCardProps> = ({ name, score }) => {
 	return (
 		<div className="grid h-12 w-5/6 grid-cols-12 items-center justify-start gap-4 border-b">
 			<div className="col-span-2 md:col-span-1 flex items-center">
-				<FaMedal className="ml-1 text-yellow-300" />
+				<FaMedal className="ml-1 text-yellow-300 md:text-base" />
 			</div>
 			<div className="col-span-2 flex items-center gap-1">
 				<div className=" h-4 w-4 md:h-8 md:w-8 rounded-full bg-gradient-to-b from-gradientYellow via-gradientOrange to-gradientBlue"></div>
