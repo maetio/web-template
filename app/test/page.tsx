dcf41a7f428b57358007d0a6a7c683d725dfcbf5
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2023-07-13 16:02:16 -0400

    changed fetch functions to baseURL

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { BaseURL } from "config/constants";
+
 /**
  * Server component that displays the profile screen
  *
@@ -8,10 +10,9 @@
  */
 export default function Profile() {
 	const handlePress = async () => {
-		const thing = await fetch(
-			"http://localhost:3000/api/stripe/create-stripe-account/PKSlgS5QTif09Pp2cC6ovqMWPvt1",
-			{ method: "POST" }
-		);
+		const thing = await fetch(`${BaseURL}/PKSlgS5QTif09Pp2cC6ovqMWPvt1`, {
+			method: "POST",
+		});
 		console.log("thing", thing);
 		const otherThing = await thing.json();
 		console.log("thingy", otherThing);
