abdc1699503b2ca3d33338166619291742fd0798
toni-akintola <oakintol@nd.edu>
2023-07-18 11:27:08 -0400

    made location ui conditional

--- app/components/cards/competition-card.tsx
+++ app/components/cards/competition-card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaBasketball, FaLocationArrow, FaRegCalendar } from "react-icons/fa6";
+import { Competition } from "types/index";
 import { TeamCard } from "./team-card";
 import { PlayerCard } from "./player-card";
 
@@ -10,16 +11,12 @@ React.DetailedHTMLProps<
 >,
 "color"
 >{
-    competitionName?: string
-	sport?: string
-	competitionType?: string
-	competitionStart?: string
-	location?: string
+    competition: Partial<Competition>
 	price?: string
 	width?: number
 }
 
-const CompetitionCard: React.FC<CompetitionCardProps> = ({competitionName, sport, competitionType, competitionStart, location, price, width, ...divParams }) => {
+const CompetitionCard: React.FC<CompetitionCardProps> = ({competition, price, width, ...divParams }) => {
 	
 	return (
 		<div {...divParams} className={`border w-${String(width) || "auto" } rounded-md shadow-lg lg:w-2/3 mt-3 p-2 min-w-full`}>
@@ -29,19 +26,20 @@ const CompetitionCard: React.FC<CompetitionCardProps> = ({competitionName, sport
 						<div className="2xl:w-36 2xl:h-36 lg:h-24 lg:w-24 lg:min-h-0 w-12 h-12 rounded-md bg-gradient-to-b from-gradientYellow via-gradientOrange to-gradientBlue"></div>
 					</div>
 					<div className="col-span-7 flex-col flex">
-						<h2 className="font-bold lg:text-3xl sm:text-lg">{competitionName}</h2>
+						<h2 className="font-bold lg:text-3xl sm:text-lg">{competition.name}</h2>
 						<div className="flex-row flex items-center">
 							<FaBasketball className="text-gray-400 text-xs lg:text-base" />
-							<p className="text-gray-400 ml-2 text-xs lg:text-base">{sport} {competitionType}</p>
+							<p className="text-gray-400 ml-2 text-xs lg:text-base">{competition.sport} {competition.type}</p>
 						</div>
 						<div className="flex-row flex items-center">
 							<FaRegCalendar className="text-gray-400 text-xs lg:text-base" />
-							<p className="text-gray-400 ml-2 text-xs lg:text-base">{competitionStart}</p>
+							<p className="text-gray-400 ml-2 text-xs lg:text-base">{competition.startTimeISO}</p>
 						</div>
+						{competition.location && 
 						<div className="flex-row flex items-center">
 							<FaLocationArrow className="text-gray-400 text-xs lg:text-base" />
-							<p className="text-gray-400 ml-2 text-xs lg:text-base">{location}</p>
-						</div>
+							<p className="text-gray-400 ml-2 text-xs lg:text-base">{competition.location?.address}</p>
+						</div>}
 					</div>
 					<div className="col-span-3 flex flex-row items-start justify-end">
 						<p className="lg:text-2xl text-gray-400 text-xs">$99</p>
