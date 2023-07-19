d01d1219425b2ad18cdc93d7c29db5ba42193994
toni-akintola <oakintol@nd.edu>
2023-07-19 10:57:00 -0400

    mostly fixed header sizing

--- app/components/cards/competition-card.tsx
+++ app/components/cards/competition-card.tsx
@@ -39,7 +39,7 @@ const CompetitionCard: React.FC<CompetitionCardProps> = ({competition, price, wi
 							style={{
 								backgroundImage: competition?.image ? `url(${competition?.image})` : "bg-none"
 							}}
-							className="2xl:w-36 2xl:h-36 lg:h-24 lg:w-24 lg:min-h-0 w-16 h-16 rounded-md bg-gradient-to-b from-gradientYellow via-gradientOrange to-gradientBlue"></div>
+							className="2xl:w-36 2xl:h-36 lg:h-24 lg:w-24 lg:min-h-0 sm:w-16 sm:h-16 w-12 h-12 rounded-md bg-gradient-to-b from-gradientYellow via-gradientOrange to-gradientBlue"></div>
 					</div>
 					<div className="col-span-7 flex-col flex">
 						<h2 className="font-bold lg:text-3xl sm:text-lg">{competition.name}</h2>
---
+++ app/components/tailwind-header.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { HiBars3, HiBell, HiXMark, HiMagnifyingGlass } from 'react-icons/hi2'
+import { HeaderMaetIcon } from './icons';
+import Link from 'next/link';
+import { ProfileButton } from './profile-button';
+
+
+export default function Example() {
+  return (
+    <Disclosure as="header" className="bg-white shadow">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:divide-y lg:divide-gray-200 lg:px-8">
+            <div className="relative flex h-16 justify-between">
+              <div className="relative z-10 flex px-2 lg:px-0">
+                <div className="flex flex-shrink-0 items-center">
+                <Link href="/">
+				    <div className="items center flex">
+					    <HeaderMaetIcon />
+					    <p className="ml-2 text-lg text-primaryMain">Maet</p>
+				    </div>
+			    </Link>
+                </div>
+              </div>
+              <div className="relative z-0 flex flex-1 items-center justify-center px-2 sm:absolute sm:inset-0">
+                <div className="w-full sm:max-w-xs">
+                  <label htmlFor="search" className="sr-only">
+                    Search
+                  </label>
+                  <div className="relative">
+                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                      <HiMagnifyingGlass className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                    </div>
+                    <input
+                      id="search"
+                      name="search"
+                      className="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      placeholder="Search"
+                      type="search"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="relative z-10 flex items-center lg:hidden">
+                <ProfileButton />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </Disclosure>
+  )
+}
