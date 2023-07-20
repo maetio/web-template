7bcdc7053593f9752459cded10a80317462f2386
toni-akintola <oakintol@nd.edu>
2023-07-20 16:35:02 -0400

    reset skeletonCard

--- app/components/cards/skeleton-card.tsx
+++ app/components/cards/skeleton-card.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 
-export type SkeletonAnimationProps = {}
+export type SkeletonCardProps = {}
 
-const SkeletonAnimation: React.FC<SkeletonAnimationProps> = () => {
+const SkeletonCard: React.FC<SkeletonCardProps> = () => {
 	return (
-		<div className="w-full mt-3 animate-pulse flex-row items-center justify-center space-x-1 p-6 h-auto ">
-			<div className="flex flex-col space-y-28 items-center">
-				<div className="h-9 w-11/12 rounded-md bg-gray-300 "></div>
-				<div className="h-9 w-2/3 rounded-md bg-gray-300 "></div>
-				<div className="h-9 w-3/4 rounded-md bg-gray-300 "></div>
-				<div className="h-9 w-1/3 rounded-md bg-gray-300 "></div>
-				<div className="h-9 w-1/4 rounded-md bg-gray-300 "></div>
-				<div className="h-9 w-1/2 rounded-md bg-gray-300 "></div>
-				
+		<div className="w-full mt-3 animate-pulse flex-row items-center justify-center space-x-1 rounded-xl border p-6 h-auto ">
+			<div className="flex flex-col space-y-6">
+				<div className="h-6 w-1/3 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-1/2 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-2/3 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-11/12 rounded-md bg-gray-300 "></div>
 			</div>
 		</div>
 	);
 };
 
-export default SkeletonAnimation;
+export default SkeletonCard;
