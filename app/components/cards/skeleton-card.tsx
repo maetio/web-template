2afca1f279814ee21489b2b50b0a1cf62b355f1f
toni-akintola <oakintol@nd.edu>
2023-07-20 15:02:22 -0400

    made skeleton card w/ maet logo pulse

--- app/components/cards/skeleton-card.tsx
+++ app/components/cards/skeleton-card.tsx
@@ -4,12 +4,32 @@ export type SkeletonCardProps = {}
 
 const SkeletonCard: React.FC<SkeletonCardProps> = () => {
 	return (
-		<div className="w-full mt-3 animate-pulse flex-row items-center justify-center space-x-1 rounded-xl border p-6 h-auto ">
-			<div className="flex flex-col space-y-6">
-				<div className="h-6 w-1/3 rounded-md bg-gray-300 "></div>
-				<div className="h-6 w-1/2 rounded-md bg-gray-300 "></div>
-				<div className="h-6 w-2/3 rounded-md bg-gray-300 "></div>
+		<div className="w-full mt-3 animate-pulse flex-row items-center justify-center space-x-1 p-6 h-auto ">
+			<div className="flex flex-col space-y-6 items-center">
 				<div className="h-6 w-11/12 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-11/12 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-11/12 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-11/12 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-2/3 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-2/3 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-2/3 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-2/3 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-3/4 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-3/4 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-3/4 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-3/4 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-1/3 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-1/3 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-1/3 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-1/4 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-1/4 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-1/4 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-1/4 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-1/2 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-1/2 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-1/2 rounded-md bg-gray-300 "></div>
+				<div className="h-6 w-1/2 rounded-md bg-gray-300 "></div>
+				
 			</div>
 		</div>
 	);
