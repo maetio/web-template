f6dd98ee5f3ba9f0e8c3a42014ba3afea8f3bd37
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2023-07-21 10:25:30 -0400

    added stripe payment success webhook to next 'testing'

---
+++ app/api/stripe/webhooks/stripePaymentIntentSuccess/route.ts
@@ -0,0 +1,88 @@
+import { BaseURL } from "config/constants";
+import {
+	competitionProfilesSubcollection,
+	competitionsCollection,
+} from "config/server";
+import { InitialRating } from "constants/rating";
+import { NextResponse, NextRequest } from "next/server";
+import Stripe from "stripe";
+import { PlayerResponseType } from "types/next-api";
+import { CompetitionProfile } from "types/profile";
+
+const STRIPE_SECRET = process.env.STRIPE_SECRET as string;
+
+const stripe = new Stripe(STRIPE_SECRET, {
+	apiVersion: "2022-11-15",
+});
+
+const STRIPE_ENDPOINT_SECRET_INTENT_BALANCE = process.env
+	.STRIPE_ENDPOINT_SECRET_INTENT_BALANCE as string;
+
+export async function POST(
+	req: NextRequest
+	// res: NextResponse
+) {
+	const sig = req.headers.get("stripe-signature");
+
+	let event;
+
+	try {
+		const body = await req.json();
+		if (sig) {
+			event = stripe.webhooks.constructEvent(
+				body,
+				sig,
+				STRIPE_ENDPOINT_SECRET_INTENT_BALANCE
+			);
+
+			if (event && event.type === "payment_intent.succeeded") {
+				const paymentIntentSucceeded = event.data
+					.object as Stripe.Response<Stripe.PaymentIntent>;
+				const { userID, compID } = paymentIntentSucceeded.metadata;
+				const compInfoRef = await competitionsCollection
+					.doc(compID)
+					.get();
+				const compInfo = compInfoRef.data();
+
+				// get initial profile
+				const profileResponse = await fetch(
+					`${BaseURL}/api/player/${userID}/${compInfo?.sport}`
+				);
+				const profileData: PlayerResponseType =
+					await profileResponse.json();
+
+				if (
+					profileData &&
+					profileData.userID &&
+					typeof paymentIntentSucceeded.latest_charge === "string"
+				) {
+					// add the profile to the competition
+					const competitionProfile: CompetitionProfile = {
+						firstName: profileData.firstName || null,
+						lastName: profileData.lastName || null,
+						image: profileData.image || null,
+						userID: profileData.userID || userID,
+						type: "player",
+						sport: compInfo?.sport || "basketball",
+						deltaRating: profileData.deltaRating,
+						rating: profileData.rating || InitialRating,
+						...profileData,
+						teamID: "daw",
+						profileID: profileData.id,
+						competitionID: compID,
+						competitionEndTimeISO: null,
+						teamFirstName: "dwa",
+						teamLastName: "dawawd",
+						stripeChargeID: paymentIntentSucceeded.latest_charge,
+						// endTimestamp: compInfo?.endTimestamp || null, // need to change to create a new timestamp
+					};
+					await competitionProfilesSubcollection(compID)
+						.doc(profileData.id)
+						.set(competitionProfile, { merge: true });
+				}
+			}
+		}
+	} catch (err: any) {
+		NextResponse.json({ received: true });
+	}
+}
--- next.config.js
+++ next.config.js
@@ -5,4 +5,7 @@ module.exports = {
 	experimental: {
 		serverActions: true,
 	},
+	api: {
+		bodyParser: false,
+	},
 };
