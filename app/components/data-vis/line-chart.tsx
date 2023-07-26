426e592cb042ef922df0c9d105eedc397b30e74a
toni-akintola <oakintol@nd.edu>
2023-07-26 13:54:00 -0400

    working through chart component

--- app/components/data-vis/line-chart.tsx
+++ app/components/data-vis/line-chart.tsx
@@ -5,11 +5,30 @@ import "chart.js/auto";
 import { Chart } from "react-chartjs-2";
 
 export interface LineChartProps {
-    labels?: string[];
-    data?: number[];
+    title?: string
+    labels?: Array<string | undefined>;
+    dataset?: Array<number | undefined>;
 }
   
-const LineChart: React.FC<LineChartProps> = (labels, data) => {
+const LineChart: React.FC<LineChartProps> = ({title, labels, dataset}) => {
+	const data = {
+		labels,
+		// datasets is an array of objects where each object represents a set of data to display corresponding to the labels above. for brevity, we'll keep it at one object
+		datasets: [
+			{
+				label: title,
+				data: dataset,
+				// you can set individual colors for each bar
+				backgroundColor: [
+					"rgba(255, 255, 255, 0.6)",
+					"rgba(255, 255, 255, 0.6)",
+					"rgba(255, 255, 255, 0.6)"
+				],
+				borderWidth: 1,
+			}
+		]
+	};
+
 	return (
 		<div>
 			<Chart type="line" data={data} />
