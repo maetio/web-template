4162cbcddd5eb22d3e5001a1abbb6304ce709f5a
toni-akintola <oakintol@nd.edu>
2023-07-26 16:52:31 -0400

    prettier looking visualization with tooltips

--- app/components/data-vis/line-chart.tsx
+++ app/components/data-vis/line-chart.tsx
@@ -5,33 +5,63 @@ import "chart.js/auto";
 import { Chart } from "react-chartjs-2";
 
 export interface LineChartProps {
-    title?: string
-    labels?: Array<string | undefined>;
+    title?: string;
     dataset?: Array<number | undefined>;
+    labels?: Array<string | undefined>; 
+    variable?: string;
 }
   
-const LineChart: React.FC<LineChartProps> = ({title, labels, dataset}) => {
+const LineChart: React.FC<LineChartProps> = ({title, dataset, labels, variable}) => {
 	const data = {
-		labels,
+		labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
 		// datasets is an array of objects where each object represents a set of data to display corresponding to the labels above. for brevity, we'll keep it at one object
 		datasets: [
 			{
-				label: title,
+				label: variable,
 				data: dataset,
+				lineTension: 0.6,
 				// you can set individual colors for each bar
-				backgroundColor: [
-					"rgba(255, 255, 255, 0.6)",
-					"rgba(255, 255, 255, 0.6)",
-					"rgba(255, 255, 255, 0.6)"
-				],
+				backgroundColor:
+					"rgb(129, 140, 248, 0.3)"
+				,
 				borderWidth: 1,
+				borderColor: "rgb(129, 140, 248)",
+				fill: true
+
 			}
 		]
+          
+	};
+
+	const options = {
+		responsive: true,
+		plugins: {
+			legend: {
+				labels: {
+					font: {
+						family: "Nunito"
+					}
+				},
+				position: "top" as const,
+			},
+			title: {
+				display: true,
+				text: title,
+			},
+		},
+		scales: {
+			x: {
+				display: false
+			},
+			y: {
+				display: false
+			}
+		}
 	};
 
 	return (
 		<div>
-			<Chart type="line" data={data} />
+			<Chart type="line" data={data} options={options} />
 		</div>
 	);
 };
