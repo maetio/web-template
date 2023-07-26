0e97c67b9554d78b526995a47e04d78bdedf24d5
toni-akintola <oakintol@nd.edu>
2023-07-26 13:18:55 -0400

    added types for line chart component

--- app/components/data-vis/line-chart.tsx
+++ app/components/data-vis/line-chart.tsx
@@ -1,29 +1,18 @@
+"use client";
+
 import React from "react";
-import { Line } from "react-chartjs-2";
+import "chart.js/auto";
+import { Chart } from "react-chartjs-2";
 
+export interface LineChartProps {
+    labels?: string[];
+    data?: number[];
+}
   
-const LineChart: React.FC<{}> = () => {
-	const data = {
-		labels: ["Red", "Orange", "Blue"],
-		// datasets is an array of objects where each object represents a set of data to display corresponding to the labels above. for brevity, we'll keep it at one object
-		datasets: [
-			{
-				label: "Popularity of colours",
-				data: [55, 23, 96],
-				// you can set indiviual colors for each bar
-				backgroundColor: [
-					"rgba(255, 255, 255, 0.6)",
-					"rgba(255, 255, 255, 0.6)",
-					"rgba(255, 255, 255, 0.6)"
-				],
-				borderWidth: 1,
-			}
-		]
-	};
-
+const LineChart: React.FC<LineChartProps> = (labels, data) => {
 	return (
 		<div>
-			<Line data={data} />
+			<Chart type="line" data={data} />
 		</div>
 	);
 };
