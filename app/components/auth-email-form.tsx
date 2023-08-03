adf2df8332ba982fd021737ea36b64041ec960c9
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2023-08-03 17:30:05 -0400

    add sign in and sign up form

--- app/components/auth-email-form.tsx
+++ app/components/auth-email-form.tsx
@@ -42,13 +42,13 @@ const AuthEmailForm: React.FC<{ redirectURL?: string }> = ({ redirectURL }) => {
 		else router.push("/profile");
 	};
 
-	const facebookSignIn = async () => {
-		const userCredential = await signInWithFacebook();
-		// route to new page
-		if (userCredential.user.displayName?.length)
-			router.push(redirectURL || "/");
-		else router.push("/profile");
-	};
+	// const facebookSignIn = async () => {
+	// 	const userCredential = await signInWithFacebook();
+	// 	// route to new page
+	// 	if (userCredential.user.displayName?.length)
+	// 		router.push(redirectURL || "/");
+	// 	else router.push("/profile");
+	// };
 
 	const signInGuest = async () => {
 		const userCredential = await signInAsGuest();
@@ -60,71 +60,20 @@ const AuthEmailForm: React.FC<{ redirectURL?: string }> = ({ redirectURL }) => {
 
 	return (
 		<>
-			<div className="flex h-full min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-				<div className="align-center justify-center sm:mx-auto sm:w-full sm:max-w-sm">
-					<MaetIcon
-						size={20}
-						className="align-center mx-auto w-20 justify-center"
-					/>
-					<h2 className="mt-7 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-						Welcome to Maet!
-					</h2>
-				</div>
-				<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-					<ActionButton
-						className="my-4 w-full"
-						startIcon="google"
-						action={googleSignIn}
-						title="Continue with Google"
-					/>
-					<ActionButton
-						className="my-4 w-full"
-						startIcon="facebook"
-						action={facebookSignIn}
-						title="Continue with Facebook"
-					/>
-					<ActionButton
-						className="my-4 w-full"
-						action={signInGuest}
-						title="Continue as Guest"
-					/>
-					{/* <ActionButton
-						className="w-full my-4"
-						title="Use Email and Password"
-						referRoute="/create-account"
-					/> */}
-				</div>
+			<div className="mt-6 grid grid-cols-2 gap-4">
+				<ActionButton
+					className="my-4 w-full"
+					startIcon="google"
+					action={googleSignIn}
+					title="Google"
+				/>
+				<ActionButton
+					className="my-4 w-full"
+					// startIcon="person"
+					action={signInGuest}
+					title="Guest"
+				/>
 			</div>
 		</>
-		// <div className="flex max-h-full max-w-full items-center">
-		// 	<div className="inline-block h-40 items-center justify-center bg-lightGray">
-		// 		<SubmitFormActionButton action={onSubmit} title="Sign in with Google" />
-		// 		<form
-		// 			onSubmit={handleSubmit(submitEmail)}
-		// 			className="flex items-center gap-2"
-		// 		>
-		// 			<input
-		// 				// eslint-disable-next-line react/jsx-props-no-spreading
-		// 				{...register("email")}
-		// 				required
-		// 				type="text"
-		// 				name="email"
-		// 				className="w-50 flex-grow rounded-lg border p-1 text-2xl"
-		// 				placeholder="Enter Email"
-		// 				autoFocus
-		// 			/>
-		// 			{sentEmail ? (
-		// 				<div>Sent!</div>
-		// 			) : (
-		// 				<button
-		// 					type="submit"
-		// 					className="mr-4 mt-4 max-w-xs rounded-2xl border-2 border-solid border-black bg-primaryMain p-2 text-xl text-black hover:cursor-pointer hover:bg-primaryMainLight"
-		// 				>
-		// 					Send Magic Link
-		// 				</button>
-		// 			)}
-		// 		</form>
-		// 	</div>
-		// </div>
 	);
 };
