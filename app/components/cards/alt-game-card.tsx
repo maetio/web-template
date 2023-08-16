d5231fe1495c627d754903926c2b32862cfacf4d
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2023-08-16 10:46:46 -0400

    game card now wraps

--- app/components/cards/alt-game-card.tsx
+++ app/components/cards/alt-game-card.tsx
@@ -66,35 +66,39 @@ const GameCard: React.FC<GameCardProps> = async ({
 	return (
 		<div
 			{...divParams}
-			className="mt-4 min-w-full flex-col justify-start  justify-evenly rounded-xl  border bg-white p-4 align-top shadow-lg"
+			className="mt-4 flex flex-wrap min-w-full rounded-xl border bg-white p-4 shadow-lg"
 		>
 			{/* time section */}
-			<section className="self-start">
+			<section className="mr-3 self-center whitespace-nowrap">
 				<div className="mt-1 items-center">
 					<p className="text-3xl font-semibold text-black">
-						{getShortDateString(new Date(game.startTimeISO || ""))}{" "}
+						{getShortDateString(new Date(game.startTimeISO || ""))}
 					</p>
 					<p className="text-sm font-semibold text-gray-500">
 						at {getTimeString(new Date(game.startTimeISO || ""))}
 					</p>
 				</div>
 			</section>
-			{/* main game content section */}
-			<section className=" mb-1 flex flex-row justify-evenly py-2">
-				<div className="flex flex-col items-center justify-center">
-					<div className=" isolate flex flex overflow-hidden">
-						{game &&
-						game.team1?.image &&
-						typeof game.team1?.image !== "string" ? (
-								game.team1.image.slice(0, 2).map((img, index) => (
-									<NextImage
-										key={index}
-										// className="h-12 w-12 flex-none rounded-md bg-gray-50 sm:h-16 sm:w-16 lg:h-24 lg:min-h-0 lg:w-24 2xl:h-36 2xl:w-36"
-										className="relative z-0 inline-block h-[35px] w-[35px] rounded-full ring-2 ring-white"
-										src={img}
-										alt="player image"
-									/>
-								))
+
+			<div className=" flex-col flex-grow">
+				{/* main game content section */}
+				<section className=" mb-1 flex flex-row justify-between py-2">
+					<div className="flex flex-row items-center justify-center">
+						<div className=" isolate flex overflow-hidden">
+							{game &&
+							game.team1?.image &&
+							typeof game.team1?.image !== "string" ? (
+								game.team1.image
+									.slice(0, 2)
+									.map((img, index) => (
+										<NextImage
+											key={index}
+											// className="h-12 w-12 flex-none rounded-md bg-gray-50 sm:h-16 sm:w-16 lg:h-24 lg:min-h-0 lg:w-24 2xl:h-36 2xl:w-36"
+											className="relative z-0 inline-block h-[35px] w-[35px] rounded-full ring-2 ring-white"
+											src={img}
+											alt="player image"
+										/>
+									))
 							) : (
 								<NextImage
 									className="h-12 w-12 flex-none rounded-md bg-gray-50 sm:h-16 sm:w-16 lg:h-24 lg:min-h-0 lg:w-24 2xl:h-36 2xl:w-36"
@@ -106,110 +110,113 @@ const GameCard: React.FC<GameCardProps> = async ({
 									alt=""
 								/>
 							)}
-						{/* {game.team1?.image &&
+							{/* {game.team1?.image &&
 							typeof game.team1.image !== "string" &&
 							game.team1.image?.length - 2 > 0 && (
 								<div className="relative z-0 inline-block flex h-[35px] w-[35px] items-center justify-center rounded-full bg-slate-50 ring-2 ring-white">
 									+{game.team1.image?.length - 2}
 								</div>
 							)} */}
-					</div>
-					<div
-						className={`flex flex-col justify-center ${
-							gameStatus === "team1-winner"
-								? "text-black"
-								: "text-gray-500"
-						}`}
-					>
-						<p className="p-2 text-center text-xs font-semibold lg:text-sm">
-							{game.team1?.lastName}
-						</p>
-						<div className="flex items-center justify-center">
-							{gameStatus === "team1-winner" ? (
-								<XSMaetIcon />
-							) : (
-								<XSGrayMaetIcon />
-							)}
-							<p className="ml-1 text-xs">
-								{Math.round(
-									game.team1?.rating?.displayRating || 100
-								)}
-							</p>
 						</div>
-						{/* points awarded */}
-						{gameStatus !== "unreported" && team1PointsAwarded ? (
-							<div className="mt-1 items-center">
-								<p
-									className={`px-auto rounded-3xl text-center text-sm font-bold ${
-										team1PointsAwarded > 0
-											? "bg-green-200 text-green-800"
-											: "bg-red-200 text-red-800"
-									}`}
-								>
-									{team1PointsAwarded > 0 && "+"}
-									{Math.round(team1PointsAwarded)}
-								</p>
-								<p>
-									{team1PointsAwarded > 0
-										? "Points Won"
-										: "Points Lost"}
+						<div
+							className={`flex flex-col justify-center ${
+								gameStatus === "team1-winner"
+									? "text-black"
+									: "text-gray-500"
+							}`}
+						>
+							<p className="p-2 text-center text-xs font-semibold lg:text-sm">
+								{game.team1?.lastName}
+							</p>
+							<div className="flex items-center justify-center">
+								{gameStatus === "team1-winner" ? (
+									<XSMaetIcon />
+								) : (
+									<XSGrayMaetIcon />
+								)}
+								<p className="ml-1 text-xs">
+									{Math.round(
+										game.team1?.rating?.displayRating || 100
+									)}
 								</p>
 							</div>
-						) : null}
-					</div>
-				</div>
-				<div className="flex flex-col items-start gap-8">
-					<div className="flex min-w-full items-center justify-center">
-						{gameStatus !== "unreported" ? (
-							<div className="flex items-center justify-center whitespace-nowrap rounded-2xl bg-zinc-100 p-2 sm:px-6 sm:py-3.5">
-								<p className="text-1xl font-bold leading-tight tracking-tight text-gray-500 md:text-3xl ">
-									<span
-										className={
-											gameStatus === "team1-winner"
-												? "font-bold text-black"
-												: ""
-										}
+							{/* points awarded */}
+							{gameStatus !== "unreported" &&
+							team1PointsAwarded ? (
+								<div className="mt-1 items-center">
+									<p
+										className={`px-auto rounded-3xl text-center text-sm font-bold ${
+											team1PointsAwarded > 0
+												? "bg-green-200 text-green-800"
+												: "bg-red-200 text-red-800"
+										}`}
 									>
-										{game.team1?.points}
-									</span>{" "}
-									:{" "}
-									<span
-										className={
-											gameStatus === "team2-winner"
-												? "font-bold text-black"
-												: ""
-										}
-									>
-										{game.team2?.points}
-									</span>
-								</p>
-							</div>
-						) : (
-							<div className="inline-flex items-center justify-center self-center rounded-2xl bg-zinc-100 px-3.5 py-2.5 sm:px-6 sm:py-3.5">
-								<text
-									aria-label="Versus"
-									className="text-1xl text-center font-bold leading-tight tracking-tight text-black md:text-3xl"
-								>
-									VS
-								</text>
-							</div>
-						)}
+										{team1PointsAwarded > 0 && "+"}
+										{Math.round(team1PointsAwarded)}
+									</p>
+									<p>
+										{team1PointsAwarded > 0
+											? "Points Won"
+											: "Points Lost"}
+									</p>
+								</div>
+							) : null}
+						</div>
 					</div>
-				</div>
-				<div className="flex flex-col items-center justify-center">
-					<div className=" isolate flex flex overflow-hidden">
-						{game &&
-						game.team2?.image &&
-						typeof game.team2?.image !== "string" ? (
-								game.team2.image.slice(0, 2).map((img, index) => (
-									<NextImage
-										key={index}
-										// className="h-12 w-12 flex-none rounded-md bg-gray-50 sm:h-16 sm:w-16 lg:h-24 lg:min-h-0 lg:w-24 2xl:h-36 2xl:w-36"
-										className="relative z-0 inline-block h-[35px] w-[35px] rounded-full ring-2 ring-white"
-										src={img}
-										alt="player image"
-									/>
-								))
+					<div className="flex flex-col items-start gap-8">
+						<div className="flex min-w-full items-center justify-center">
+							{gameStatus !== "unreported" ? (
+								<div className="flex items-center justify-center whitespace-nowrap rounded-2xl bg-zinc-100 p-2 sm:px-6 sm:py-3.5">
+									<p className="text-1xl font-bold leading-tight tracking-tight text-gray-500 md:text-3xl ">
+										<span
+											className={
+												gameStatus === "team1-winner"
+													? "font-bold text-black"
+													: ""
+											}
+										>
+											{game.team1?.points}
+										</span>{" "}
+										:{" "}
+										<span
+											className={
+												gameStatus === "team2-winner"
+													? "font-bold text-black"
+													: ""
+											}
+										>
+											{game.team2?.points}
+										</span>
+									</p>
+								</div>
+							) : (
+								<div className="inline-flex items-center justify-center self-center rounded-2xl bg-zinc-100 px-3.5 py-2.5 sm:px-6 sm:py-3.5">
+									<text
+										aria-label="Versus"
+										className="text-1xl text-center font-bold leading-tight tracking-tight text-black md:text-3xl"
+									>
+										VS
+									</text>
+								</div>
+							)}
+						</div>
+					</div>
+					<div className="flex flex-row-reverse items-center justify-center">
+						<div className=" isolate flex overflow-hidden">
+							{game &&
+							game.team2?.image &&
+							typeof game.team2?.image !== "string" ? (
+								game.team2.image
+									.slice(0, 2)
+									.map((img, index) => (
+										<NextImage
+											key={index}
+											// className="h-12 w-12 flex-none rounded-md bg-gray-50 sm:h-16 sm:w-16 lg:h-24 lg:min-h-0 lg:w-24 2xl:h-36 2xl:w-36"
+											className="relative z-0 inline-block h-[35px] w-[35px] rounded-full ring-2 ring-white"
+											src={img}
+											alt="player image"
+										/>
+									))
 							) : (
 								<NextImage
 									className="h-12 w-12 flex-none rounded-md bg-gray-50 sm:h-16 sm:w-16 lg:h-24 lg:min-h-0 lg:w-24 2xl:h-36 2xl:w-36"
@@ -221,72 +228,73 @@ const GameCard: React.FC<GameCardProps> = async ({
 									alt=""
 								/>
 							)}
-						{/* {game.team2?.image &&
+							{/* {game.team2?.image &&
 							typeof game.team2.image !== "string" &&
 							game.team2.image?.length - 2 > 0 && (
 								<div className="relative z-0 inline-block flex h-[35px] w-[35px] items-center justify-center rounded-full bg-slate-50 ring-2 ring-white">
 									+{game.team2.image?.length - 2}
 								</div>
 							)} */}
-					</div>
-
-					<div
-						className={`flex flex-col justify-center ${
-							gameStatus === "team2-winner"
-								? "text-black"
-								: "text-gray-500"
-						}`}
-					>
-						<p className="p-2 text-center text-xs font-semibold lg:text-sm">
-							{game.team2?.lastName}
-						</p>
-						<div className="flex items-center justify-center">
-							{gameStatus === "team2-winner" ? (
-								<XSMaetIcon />
-							) : (
-								<XSGrayMaetIcon />
-							)}
-							<p className="ml-1 text-xs">
-								{Math.round(
-									game.team2?.rating?.displayRating || 100
-								)}
-							</p>
 						</div>
-						{/* points awarded */}
-						{gameStatus !== "unreported" && team2PointsAwarded ? (
-							<div className="mt-1 items-center">
-								<p
-									className={`px-auto rounded-3xl text-center text-sm font-bold ${
-										team2PointsAwarded > 0
-											? "bg-green-200 text-green-800"
-											: "bg-red-200 text-red-800"
-									}`}
-								>
-									{team2PointsAwarded > 0 && "+"}
-									{Math.round(team2PointsAwarded)}
-								</p>
-								<p>
-									{team2PointsAwarded > 0
-										? "Points Won"
-										: "Points Lost"}
+
+						<div
+							className={`flex flex-col justify-center ${
+								gameStatus === "team2-winner"
+									? "text-black"
+									: "text-gray-500"
+							}`}
+						>
+							<p className="p-2 text-center text-xs font-semibold lg:text-sm">
+								{game.team2?.lastName}
+							</p>
+							<div className="flex items-center justify-center">
+								{gameStatus === "team2-winner" ? (
+									<XSMaetIcon />
+								) : (
+									<XSGrayMaetIcon />
+								)}
+								<p className="ml-1 text-xs">
+									{Math.round(
+										game.team2?.rating?.displayRating || 100
+									)}
 								</p>
 							</div>
-						) : null}
+							{/* points awarded */}
+							{gameStatus !== "unreported" &&
+							team2PointsAwarded ? (
+								<div className="mt-1 items-center">
+									<p
+										className={`px-auto rounded-3xl text-center text-sm font-bold ${
+											team2PointsAwarded > 0
+												? "bg-green-200 text-green-800"
+												: "bg-red-200 text-red-800"
+										}`}
+									>
+										{team2PointsAwarded > 0 && "+"}
+										{Math.round(team2PointsAwarded)}
+									</p>
+									<p>
+										{team2PointsAwarded > 0
+											? "Points Won"
+											: "Points Lost"}
+									</p>
+								</div>
+							) : null}
+						</div>
 					</div>
-				</div>
-			</section>
+				</section>
+				{/* win prob */}
+				{game.team1?.rating &&
+					game.team2?.rating &&
+					gameStatus === "unreported" && (
+						<WinProb
+							team1Rating={game.team1?.rating}
+							team2Rating={game.team2?.rating}
+						/>
+					)}
 
-			{/* win prob */}
-			{game.team1?.rating &&
-				game.team2?.rating &&
-				gameStatus === "unreported" && (
-				<WinProb
-					team1Rating={game.team1?.rating}
-					team2Rating={game.team2?.rating}
-				/>
-			)}
-
-			{/* win prob end */}
+				{/* win prob end */}
+			</div>
 		</div>
 	);
 };
