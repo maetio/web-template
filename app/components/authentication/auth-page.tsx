d6971405a54c839f77a3a524e17cc81f95859d5d
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2023-08-21 17:32:41 -0400

    removed unused vars

--- app/components/authentication/auth-page.tsx
+++ app/components/authentication/auth-page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { BaseURL } from "config/constants";
+// import { BaseURL } from "config/constants";
 import {
 	LoginProvidersForm,
 	LoginForm,
