3f14192779dafa445c6c03fdfa9099b8c5473870
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2023-08-21 14:02:15 -0400

    formatted

--- app/components/authentication/login-form.tsx
+++ app/components/authentication/login-form.tsx
@@ -92,7 +92,7 @@ const LoginForm: React.FC<SignupFormParams> = ({
 
 				<div>
 					<ActionButton
-						className="flex w-full mt-4 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+						className="mt-4 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 						title="Sign in"
 						colorVariant="indigo"
 						isLoading={isLoading}
