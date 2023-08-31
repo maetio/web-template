61e429a1ecbf44dc4408e3bfd45705f00832bc76
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2023-08-31 12:10:11 -0500

    progress on desktop layout

--- app/view-comp/[id]/page.tsx
+++ app/view-comp/[id]/page.tsx
@@ -82,123 +82,129 @@ export default async function ViewCompScreen({
 
 	return (
 		<main className="container min-w-full px-0 sm:px-2">
-			{/* Competition image and name banner */}
-			<section className="flex h-fit flex-col flex-wrap pt-4 md:flex-row md:flex-nowrap md:gap-2.5 lg:flex-col lg:gap-0 lg:pt-12">
-				<div className="flex">
-					<NextImage
-						size={400}
-						src={competitionData?.image}
-						alt={`${competitionData?.name} profile`}
-					/>
-				</div>
-				{/* name and host section */}
-				<div className="flex w-full flex-col justify-center gap-2.5">
-					<section className="mt-2.5 flex h-full w-full flex-col flex-wrap self-center rounded-2xl bg-white p-4 md:mt-0">
-						<h1 className="flex flex-wrap text-3xl font-bold lg:text-4xl">
-							{competitionData?.name}
-						</h1>
-						<PlayerCard host player={hostData} />
+			<div className="w-full lg:flex lg:flex-row lg:gap-2.5">
+				<div>
+					{/* Competition image and name banner */}
+					<section className="flex h-fit flex-col flex-wrap pt-4 md:flex-row md:flex-nowrap md:gap-2.5 lg:flex-col">
+						<div className="md:flex">
+							<NextImage
+								size={400}
+								src={competitionData?.image}
+								alt={`${competitionData?.name} profile`}
+							/>
+						</div>
+						{/* name and host section */}
+						<div className="flex w-full flex-col justify-center gap-2.5">
+							<section className="mt-2.5 flex h-full w-full flex-col flex-wrap self-center rounded-2xl bg-white p-4 md:mt-0">
+								<h1 className="flex flex-wrap text-3xl font-bold lg:text-4xl">
+									{competitionData?.name}
+								</h1>
+								<PlayerCard host player={hostData} />
+							</section>
+							<RatedCompetitionCard className="mb-2.5 md:mb-0 md:mt-0" />
+						</div>
 					</section>
-					<RatedCompetitionCard className="mb-2.5 md:mb-0 md:mt-0" />
+
+					<section className="mt-2.5 rounded-2xl bg-white p-4">
+						<h6 className="font-bold">Competition Info</h6>
+						<CompDisplayData
+							className="mt-5"
+							type={competitionData?.type || "session"}
+							sport={competitionData?.sport || "pickleball"}
+							startTimeISO={competitionData?.startTimeISO}
+							endTimeISO={competitionData?.endTimeISO}
+							location={competitionData?.location}
+						/>
+					</section>
 				</div>
-			</section>
 
-			<section className="mt-2.5 rounded-2xl bg-white p-4">
-				<h6 className="font-bold">Competition Info</h6>
-				<CompDisplayData
-					className="mt-5"
-					type={competitionData?.type || "session"}
-					sport={competitionData?.sport || "pickleball"}
-					startTimeISO={competitionData?.startTimeISO}
-					endTimeISO={competitionData?.endTimeISO}
-					location={competitionData?.location}
-				/>
-			</section>
+				{/* main content of the page */}
+				<section className="mt-2.5 flex w-full flex-col gap-2.5">
+					<section className="rounded-2xl bg-white p-4">
+						<h6 className="font-bold">Location</h6>
+						{competitionData?.location ? (
+							<p className="mb-2.5 mt-5 flex items-center font-bold">
+								<MdLocationOn
+									className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-900"
+									aria-hidden="true"
+								/>{" "}
+								{competitionData.location.name}
+							</p>
+						) : null}
 
-			{/* main content of the page */}
-			<section className="mt-2.5 flex min-w-full flex-col gap-2.5">
-				<section className="rounded-2xl bg-white p-4">
-					<h6 className="font-bold">Location</h6>
-					{competitionData?.location ? (
-						<p className="mb-2.5 mt-5 flex items-center font-bold">
-							<MdLocationOn
-								className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-900"
-								aria-hidden="true"
-							/>{" "}
-							{competitionData.location.name}
-						</p>
-					) : null}
-
-					{competitionData?.location?.latitude &&
-					competitionData.location.longitude ? (
+						{competitionData?.location?.latitude &&
+						competitionData.location.longitude ? (
 							<SimpleMap
 								zoom={11}
 								lat={competitionData.location.latitude}
 								lng={competitionData.location.longitude}
 							/>
 						) : null}
-				</section>
+					</section>
 
-				{/* description section */}
-				<section className="rounded-2xl bg-white p-4">
-					<h6 className="font-bold">Description</h6>
-					<p className="wrap mt-5 flex flex-wrap">
-						{competitionData?.description ||
-							"ged. It was popularised in the 1960s with the release ged. It was popularised in the 1960s with the release ged. It was popularised in the 1960s with the release ged. It was popularised in the 1960s with the release"}
-					</p>
-				</section>
-				{/* graph section */}
-				<section className="rounded-2xl bg-white p-4">
-					<div>
-						<h6 className="font-bold">Players</h6>
-						<VictoryBarGraph
-							className="w-full"
-							data={filteredPlayerData}
-							tickLabels={[
-								"<1750",
-								"1751-1850",
-								"1851-1950",
-								"1951-2050",
-								">2050",
-							]}
-						/>
-					</div>
+					{/* description section */}
+					<section className="rounded-2xl bg-white p-4">
+						<h6 className="font-bold">Description</h6>
+						<p className="wrap mt-5 flex flex-wrap">
+							{competitionData?.description ||
+								"ged. It was popularised in the 1960s with the release ged. It was popularised in the 1960s with the release ged. It was popularised in the 1960s with the release ged. It was popularised in the 1960s with the release"}
+						</p>
+					</section>
+					{/* graph section */}
+					<section className="rounded-2xl bg-white p-4">
+						<div>
+							<h6 className="font-bold">Players</h6>
+							<VictoryBarGraph
+								className="w-full"
+								data={filteredPlayerData}
+								tickLabels={[
+									"<1750",
+									"1751-1850",
+									"1851-1950",
+									"1951-2050",
+									">2050",
+								]}
+							/>
+						</div>
 
-					<div className="top-8 col-span-6 h-96 rounded-lg  bg-white lg:sticky lg:top-4 lg:col-span-2">
-						<ul
-							role="list"
-							className="sticky top-0 h-96 divide-y divide-gray-100 overflow-y-auto"
-						>
-							{players.map((player, rank) => (
-								<li key={player.id} className="px-5">
-									<PlayerCard
-										key={player.id}
-										player={player}
-										ranking={rank}
-									/>
-								</li>
-							))}
+						<div className="top-8 col-span-6 h-96 rounded-lg  bg-white lg:sticky lg:top-4 lg:col-span-2">
+							<ul
+								role="list"
+								className="sticky top-0 h-96 divide-y divide-gray-100 overflow-y-auto"
+							>
+								{players.map((player, rank) => (
+									<li key={player.id} className="px-5">
+										<PlayerCard
+											key={player.id}
+											player={player}
+											ranking={rank}
+										/>
+									</li>
+								))}
+							</ul>
+						</div>
+						<PaginationList />
+					</section>
+					{/* game section */}
+					<section className="rounded-2xl bg-white p-4">
+						<h6 className="font-bold">Games</h6>
+						<ul role="list" className="">
+							{games.length ? (
+								games.map((game) => (
+									<li key={game.id} className="lg:pr-3">
+										<GameCard id={game.id} />
+									</li>
+								))
+							) : (
+								<p className=" ml-2 mt-3 text-gray-600">
+									No Games
+								</p>
+							)}
 						</ul>
-					</div>
-					<PaginationList />
+						<PaginationList />
+					</section>
 				</section>
-				{/* game section */}
-				<section className="rounded-2xl bg-white p-4">
-					<h6 className="font-bold">Games</h6>
-					<ul role="list" className="">
-						{games.length ? (
-							games.map((game) => (
-								<li key={game.id} className="lg:pr-3">
-									<GameCard id={game.id} />
-								</li>
-							))
-						) : (
-							<p className=" ml-2 mt-3 text-gray-600">No Games</p>
-						)}
-					</ul>
-					<PaginationList />
-				</section>
-			</section>
+			</div>
 		</main>
 	);
 }
