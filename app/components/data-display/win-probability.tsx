0be98fb35ea23f653bc1c56454e7d22ee4632426
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2023-09-06 08:54:35 -0400

    changed props  on win prob component

--- app/components/data-display/win-probability.tsx
+++ app/components/data-display/win-probability.tsx
@@ -1,40 +1,29 @@
 import React from "react";
-import { expectedDisplayResult } from "utils/skill-rating";
-import { Team } from "types/team";
 
 export interface WinProbParams {
-	team1Rating: Team["rating"];
-	team2Rating: Team["rating"];
+	team1Prob: number;
+	team2Prob: number;
 }
 
 export /**
  * Prebuild win probability component. Takes in team rating and returns a progress bar with their chances of winning
  *
- * @param {*} {
- * 	team1Rating,
- * 	team2Rating,
- * }
+ *
+ * @param {*} { team1Prob, team2Prob }
  * @return {*}
  */
-const WinProb: React.FC<WinProbParams> = ({ team1Rating, team2Rating }) => {
-	// for calculation, input display ratings as well
-	const probTeam1 = Math.floor(
-		expectedDisplayResult(team1Rating, team2Rating) * 100
-	);
-
-	const probTeam2 = probTeam1 !== undefined ? 100 - probTeam1 : 0;
-
+const WinProb: React.FC<WinProbParams> = ({ team1Prob, team2Prob }) => {
 	return (
 		<section className="w-full flex-col">
 			<div
 				className={`h-2.5 w-full rounded-full ${
-					probTeam2 >= probTeam1 ? "bg-indigo-600" : "bg-indigo-300"
+					team2Prob >= team1Prob ? "bg-indigo-600" : "bg-indigo-300"
 				}`}
 			>
 				<div
-					style={{ width: `${probTeam1}%` }}
+					style={{ width: `${team1Prob}%` }}
 					className={`h-2.5 rounded-full  ${
-						probTeam2 < probTeam1
+						team2Prob < team1Prob
 							? "bg-indigo-600"
 							: "bg-indigo-300"
 					}`}
@@ -42,13 +31,13 @@ const WinProb: React.FC<WinProbParams> = ({ team1Rating, team2Rating }) => {
 			</div>
 			<div className="mt-1 flex flex-row justify-between">
 				<p className="  text-sm font-bold leading-tight tracking-tight text-black">
-					{probTeam1}%
+					{team1Prob}%
 				</p>
 				<p className=" text-center text-sm font-normal leading-tight tracking-tight text-gray-500">
 					Win Probability
 				</p>
 				<p className="  text-right text-sm font-bold leading-tight tracking-tight text-black">
-					{probTeam2}%
+					{team2Prob}%
 				</p>
 			</div>
 		</section>
